49e5500c55b1c705676f3b786f1779377ca6690f
Lucian <Lucian.io@outlook.com>
2023-09-19 03:59:15 -0400

    no error

--- src/components/interface/navbar.tsx
+++ src/components/interface/navbar.tsx
@@ -1,15 +1,21 @@
 import { Link, useLocation } from 'react-router-dom';
 import PageProps from '../../utility/types/page-props';
 import { routes } from '../../utility/config/routes';
+
 export default function Navbar() {
-  const location = useLocation()
-  const homepage = location.pathname == '/'
+  const location = useLocation();
+  const homepage = location.pathname === '/';
+
   return (
-    <div className={homepage ? 'flex flex-col justify-center align-center' : 'absolute left-0 right-0'}>
+    <div className={`navbar-container ${homepage ? '' : 'absolute left-0 right-0'}`}>
       <div className="title">Lucian</div>
       <div className="nav">
         {routes.map((page: PageProps, index: number) => (
-          <Link key={index} to={page.href}>
+          <Link
+            key={index}
+            to={page.href}
+            className={`nav-link fade-out'}`}
+          >
             {page.title}
           </Link>
         ))}
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,13 +1,29 @@
 import Fireflies from "../components/interface/fireflies";
 import Navbar from "../components/interface/navbar";
+import { useState, useEffect } from "react"; // Import useEffect
 
 export default function Home() {
-  // const isMobile = GetIsMobile();
+  const [isVisible, setIsVisible] = useState(false);
+
+  // Function to toggle visibility
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible);
+  };
+
+  // Use useEffect to add a delay before setting isVisible to true
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(true);
+    }, 100); // Delay for 1 second (adjust as needed)
+    return () => clearTimeout(timer); // Clear the timer if the component unmounts
+  }, []);
+
 
   return (
     <>
-      <div className="home-layout" >
+      <div className={`home-layout ${isVisible ? 'fade-in' : 'fade-out'}`}>
         <Navbar />
+        <button onClick={toggleVisibility}>Toggle</button>
         <Fireflies />
       </div>
     </>
