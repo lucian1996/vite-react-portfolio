a4b4c6eb1d9d69aef1f8ce7ee0c86257661b7dd9
Lucian <Lucian.io@outlook.com>
2023-09-22 04:00:32 -0400

    about to style contact card

---
+++ src/components/contact-card.tsx
@@ -0,0 +1,19 @@
+
+export default function ContactCard() {
+  return (
+    <div className="contact-card">
+      <div className="contact-card-header">
+        <div className="contact-card-image-frame">
+          <div className="content-card-image">image</div>
+        </div>
+        <div className="contact-card-header-info">
+          <div className="contact-card-header-info-name">Luke Anthony</div>
+          <div className="contact-card-header-info-title">Web Developer</div>
+        </div>
+      </div>
+      <div className="contact-card-footer">
+        icon icon icon icon
+      </div>
+    </div>
+    );
+}
--- src/page/contact-page.tsx
+++ src/page/contact-page.tsx
@@ -2,8 +2,25 @@ import Fireflies from "../components/interface/fireflies";
 import Header from "../components/interface/header";
 import { motion } from "framer-motion";
 import '../utility/style/page/contact.css'
+import ContactCard from "../components/contact-card";
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 export default function ContactPage() {
+  const form = useRef<HTMLFormElement | null>(null);
+  const sendEmail = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (form.current) {
+      emailjs.sendForm('service_1dv3bdm', 'template_y0bovyn', form.current, '4det7y_L2a0fApovP')
+        .then((result) => {
+          console.log(result.text);
+        })
+        .catch((error) => {
+          console.log(error.text);
+        });
+    }
+  };
+
   return (
     <div className="layout">
       <Header />
@@ -14,10 +31,22 @@ export default function ContactPage() {
         transition={{ duration: 0.5 }}
         className="main"
       >
-        Contact
+        <div id="Contact">
+          <div className="frame">
+            <ContactCard />
+            <form ref={form} autoComplete='false' onSubmit={sendEmail}>
+              <label>name</label>
+              <input type="text" name="user_name" />
+              <label>email</label>
+              <input type="email" name="user_email" />
+              <label>message</label>
+              <textarea name="message" />
+              <input type="submit" value="Send" onClick={() => { console.log('clicked') }} className='button' />
+            </form>
+          </div>
+        </div>
       </motion.div >
       <Fireflies />
-
     </div>
-  );
+  )
 }
