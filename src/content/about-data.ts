c9338199bc4123bedd879fecd89e8b3200c777ba
Lucian <Lucian.io@outlook.com>
2023-09-25 06:29:31 -0400

    about me

--- src/content/about-data.ts
+++ src/content/about-data.ts
@@ -2,7 +2,7 @@ const aboutData = {
   name: 'Luke Anthony',
   info: ['Born June 12th 1996', 'Mason, Ohio'],
   skills: ['JavaScript', 'React.js', 'Next.js', 'Vue.js', 'Redux', 'Prisma', 'HTML', 'CSS', 'Tailwind', 'Responsive Design', 'Java', 'Spring Boot', 'PostgreSQL', 'RESTful', 'APIs', 'E/R diagrams', 'Unit Testing', 'Integration Testing', 'Git', 'Unix'],
-  summary: "As a full-stack developer, I have grown to possess  proficiency in both front-end and back-end technologies. My expertise extends to creating end-to-end applications that prioritize an optimal user experience. I'm passionate about full-stack development, skilled in designing and developing complex applications with React and Node.js."
+  summary: "I am a full-stack developer who grown into proficiency with both front-end and back-end technologies. Passsionate about creating end-to-end applications that prioritize optimal user interaction with information or tooling"
 }; 
 
 export default aboutData
